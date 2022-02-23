bc8bc624f095709e5b4f2bff032ebf971acd770a
Favori <995749721@qq.com>
2022-02-23 08:24:18 +0800

    chore: udpate

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
+  hash:true,
   title:'React Widgets',
   favicon: 'https://github.com/yuanguandong/react-widgets/blob/master/public/logo.png?raw=true',
   nodeModulesTransform: {
