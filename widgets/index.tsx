56a1f3e730f3ef61b7b319fe93dfb8b6e98974d1
Favori <995749721@qq.com>
2022-02-24 14:22:04 +0800

    chore: update

--- widgets/index.tsx
+++ widgets/index.tsx
@@ -3,6 +3,6 @@ export {default as Column} from './column';
 export {default as Guide} from './guide';
 export {default as Popular} from './popular';
 export {default as Ring} from './ring';
-export {default as Todo} from './Todo';
+export {default as Todo} from './todo';
 
 
